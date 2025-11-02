37ed77afc47e3e1e761043649d51b8a80f47cfd7
Ekki-TS <trungsonle32@gmail.com>
2025-11-02 20:36:35 +0700

    Update Lê Trung Sơn _B25DTCN204 _ LapTrinhCoBanC _ Session07 _ BTVN09.c

--- Lê Trung Sơn _B25DTCN204 _ LapTrinhCoBanC _ Session07 _ BTVN09.c
+++ Lê Trung Sơn _B25DTCN204 _ LapTrinhCoBanC _ Session07 _ BTVN09.c
@@ -13,29 +13,28 @@ int main() {
     printf("Nhap cac phan tu cua mang:\n");
     for (int i = 0; i < hang; i++) {
         for (int j = 0; j < cot; j++) {
-            printf("a[%d][%d] = ", i, j);
             scanf("%d", &a[i][j]);
         }
     }
 
-    printf("\nMang vua nhap la:\n");
+    printf("\nMang sau khi nhap la :\n");
     for (int i = 0; i < hang; i++) {
         for (int j = 0; j < cot; j++) {
-            printf("%5d", a[i][j]);
+            printf("%d ", a[i][j]);
         }
         printf("\n");
-    }__x86_64
-
-    printf("\nCac phan tu nam tren duong bien la:\n");
-    for (int i = 0; i < hang; i++) {
-        for (int j = 0; j < cot; j++) {ffff
-            if (i == 0 || i == hang - 1 || j == 0 || j == cot - 1) {
-                printf("%5d", a[i][j]);
-            }
-        }
     }
 
-    printf("\n");
+    printf("\nCac phan tu cua mang nam tren duong bien la :\n");
+    for (int i = 0; i < hang; i++) {
+        for (int j = 0; j < cot; j++) {
+            if (i == 0 || i == hang - 1 || j == 0 || j == cot - 1)
+                printf("%d ", a[i][j]);
+            else
+                printf("  "); 
+        }
+        printf("\n");
+    }
 
     return 0;
 }
